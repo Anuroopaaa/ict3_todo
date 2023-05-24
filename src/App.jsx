54656cfaf860578ae296f5442a11f78c36cfe699
Anuroopa K S <anuroopa.saji@gmail.com>
2023-05-24 17:07:22 +0530

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,52 @@
-function App() {
+import React, { useEffect, useState } from 'react'
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import { useForm } from "react-hook-form";
+import Form from "./form.jsx"
+
+
+export default function App() {
+  const { register, handleSubmit } = useForm();
+  const[todo,setTodo] = useState([]);
+  const getData = async () =>{
+  const data = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const jsonData = await data.json();
+  setTodo(jsonData)
+  }
+
+  useEffect(()=>{
+    getData();
+  },[]
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <Container className = 'text-center mt-5'>
+        <Form></Form>
+          <h1>TO DO LIST</h1>
+          <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Title</th>
+      <th>Completed</th>
+    </tr>
+  </thead>
+  <tbody>
+    {todo.map((item, index) => (
+      <tr key={index}>
+        <td><input type="checkbox" checked ={item.completed}/></td>
+        <td>{item.title}</td>
+        <td>{item.completed?"completed":"pending"}</td>
+      </tr>
+    ))}
+  </tbody>
+</Table>
+        </Container>     
+ 
+
     </div>
-  );
+  )
 }
 
-export default App;
+
+
